e1773d470bd16f1b289f0dac29796d13cd267d4c
Div685 <divyeshpatel685@gmail.com>
2021-05-13 00:46:03 +0300

    Add user Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items, except: %i[new]
+  resources :users, only: %i[show create]
 end
