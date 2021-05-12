069d9afbf11e5671d3f7e73fb12e5c20884860ad
Div685 <divyeshpatel685@gmail.com>
2021-05-12 16:37:27 +0300

    Add Associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :appointments, dependent: :destroy
+  has_many :items, through: :appointments
+
+  validates :username, presence: true, uniqueness: true
 end
