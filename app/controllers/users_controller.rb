0506d0251ecb96fcce83d8fd6712d00af6855055
Div685 <divyeshpatel685@gmail.com>
2021-05-13 11:40:56 +0300

    update user_controller
    - Added jwtToken on create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authorize_user, only: [:show]
 
   def show
-    # change this to current user instead of fining by id
-    @user = User.find(params[:id])
-    render json: @user, status: 200
+    render json: @current_user, status: 200
   end
 
   def create
     @user = User.create!(username: user_params[:username], password: user_params[:password])
     
     if @user.valid?
-      render json: { status: :created, user: user_data(@user) }, status: 201
+      token = encode_token({ user_id: @user.id })
+      render json: { status: :created, user: user_data(@user), token: token }, status: 201
     else
       render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
     end
