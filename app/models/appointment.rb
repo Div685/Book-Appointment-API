f58bc8a7f24492b35e264c589b9845be0c8751f8
Div685 <divyeshpatel685@gmail.com>
2021-05-13 17:37:07 +0300

    update appointment model
    - to remove n+1 problem by adding default scope using includes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,8 @@ class Appointment < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :date
+
+  default_scope { order('created_at DESC') }
+  default_scope { includes(:user, :item) }
+
 end
