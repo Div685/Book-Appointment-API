56c07fb3aa0bc9f4c3f7a752bcd080a9cb2b6fb5
Div685 <divyeshpatel685@gmail.com>
2021-05-13 13:33:41 +0300

    Add login controller and login route

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,27 @@
+class LoginController < ApplicationController
+  def create
+    @user = User.find_by(username: login_params[:username])
+
+    if @user&.authenticate(login_params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { logged_in: true, user: user_data(@user), token: token }
+    else
+      render json: { errors: 'invalid User' }, status: 401
+    end
+  end
+
+  private
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def user_data(user)
+    user_obj = {
+      id: user.id,
+      username: user.username,
+    }
+    user_obj
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items, except: %i[new]
   resources :users, only: %i[show create]
+  resources :login, only: %i[create]
 end
