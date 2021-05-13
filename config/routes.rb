e97af14db3ee64988e749590e2418508fa374c24
Div685 <divyeshpatel685@gmail.com>
2021-05-13 21:28:59 +0300

    update rails serve port to 3001

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-      resources :appointments, except: %i[new]
-      resources :items, except: %i[new]
       resources :users, only: %i[show create]
       resources :login, only: %i[create]
+      resources :items, except: %i[new]
+      resources :appointments, except: %i[new]
     end
   end
 end
