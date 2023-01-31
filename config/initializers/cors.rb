d723f102bac80c6604fde3f7d2546866e0b31969
Div685 <divyeshpatel685@gmail.com>
2023-01-31 11:49:44 +0300

    Update cors policy
    - Add new header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -24,4 +24,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+  
+  allow do
+    origins 'https://salon-appointment-33fe4.web.app/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
